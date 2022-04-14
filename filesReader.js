917b00ddfbd9b7a382cb6ad5b77c73df989b3862
cark666 <carlos666.cgm@gmail.com>
2022-04-14 12:59:06 +0200

    Commit message1

---
+++ filesReader.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+let path = 'text.txt';
+
+
+
+
+
+async function readFile() {
+
+    try {
+        var data = fs.readFileSync(path, 'utf8');
+        console.log(data);
+        return data;
+    } catch (e) {
+        console.log('Error:', e.stack);
+    }
+
+
+}
+
+
+
+
+module.exports = {
+    readFile
+};
+
+// fs.open(ruta, 'a', (err, fd) => {
+//     if (err) { 
+//         console.log(err); 
+//     }
+//     else {
+//         //si tienes ya el fd (file descriptor), usémoslo        
+//         fs.appendFile(fd, texto, function (err) {
+//             if (err) throw err;
+//             //de nuevo, usemos el fd para cerrar el fichero abierto
+//             fs.close(fd, function (err) {
+//                 if (err) throw err;
+//                 console.log('It\'s saved!');
+//             });
+//         });
+//     }
